3846e8352d53133f6b74334e723be60449704546
Dalton Schaadt <daltonschaadt@gmail.com>
2014-02-16 06:28:41 -0500

    Client and server can communicate with eachother, I will hopefully get a chance to test serial communication from rPi to teensy tomorrow.

--- rpiServer.py
+++ rpiServer.py
@@ -1,9 +1,67 @@
-import sys, socket, serial
+import sys, socket, serial, threading, select, time
 from thread import *
+from collections import deque
 
-HOST = ''	# symbolic name: all available interfaces
-PORT = 7268	# PORT: RBOT - lololol
+HOST = ''			# symbolic name: all available interfaces
+PORT = 7268			# PORT: RBOT - lololol
 
+serialRead  = deque()		# Que for writing to serial.
+serialWrite = deque()		# Que for reading from serial.
+
+readLock  = threading.Lock();	# Lock for serialRead.
+writeLock = threading.Lock();	# Lock for serialWrite.
+
+
+# Function for handling data recieving.
+def receiveData(conn):
+	# keep func/thread alive
+	while 1:
+		data = conn.recv(1)			# Recieve a byte at a time from the client
+		if data:				# If no data, loop again
+			writeLock.acquire()
+			serialWrite.append(data)	# Otherwise, write byte to the write que
+			writeLock.release()
+
+		time.sleep(0);
+
+	#came out of loop 
+	conn.close()
+
+
+# Function for data transmittion.
+def transmitData(conn):
+	while 1:
+		readLock.acquire()
+		if len(serialRead) > 0:				# If there is data to be sent
+			conn.sendall(serialRead.popleft())	# send it
+		readLock.release()
+
+		time.sleep(0);
+
+
+# Fucntion for serial communication with teensy
+def serialHandler():
+#	s = serial.Serial('/dev/tty.usbserial', 9600)
+
+	while 1:
+		#if s.inWaiting() > 0:					# If there is stuff to be read, read it:
+		if select.select([sys.stdin,],[],[],0.0)[0]:
+			readLock.acquire()
+#			serialRead.append(s.readline())			# Place it in the que for the transmitData thread.
+			serialRead.append(sys.stdin.readline())
+			readLock.release()
+
+		if len(serialWrite) > 0:				# If there is stuff to write from the receiveData thread:
+			writeLock.acquire()
+#			s.write(serialWrite.popleft().encode(ascii))	# Write it to the serial connection.
+			sys.stdout.write(serialWrite.popleft())
+			sys.stdout.flush()
+			writeLock.release()
+
+		time.sleep(0)
+
+
+# Main
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
 
@@ -20,7 +78,7 @@ s.listen(2)
 print 'Socket now listening.'
 
 # Create a thread to handel the serial connection
-start_new_thread(serialHandler)
+start_new_thread(serialHandler, ())
 
 # Keep accepting connections
 while 1:
@@ -41,25 +99,3 @@ while 1:
 		conn.close()
 
 s.close()
-
-
-# Function for handling data recieving.
-def receiveData(conn):
-	# keep func/thread alive
-	while 1:
-		# Receive from client
-		data = conn.recv(1024)
-		if not data:
-			break
-
-		conn.sendall(reply)
-
-	#came out of loop 
-	conn.close()
-
-# Function for data transmitting.
-def transmitData(conn):
-	asdfasdf;
-
-def serialHandler():
-	asdfasdf;
